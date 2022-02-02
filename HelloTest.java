cc2506c0db783196afa1d06ca06b7a1532bd6d97
melissaesantos <91588097+melissaesantos@users.noreply.github.com>
2022-02-02 10:40:10 -0800

    updated test

--- HelloTest.java
+++ HelloTest.java
@@ -1,10 +1,9 @@
 //import jUnit testing
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.*;import org.junit.*;
 public class HelloTest{
     //add tester
     @Test
     public void subtract(){
-        assertEquals(5, Hello.subtract(10,2));
+        assertEquals(5, Hello.subtract(10,5));
     }
 }
